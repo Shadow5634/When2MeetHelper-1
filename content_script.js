1b7217dade6b5bae0e89443a042528ae54cd453b
Shadow5634 <98679326+Shadow5634@users.noreply.github.com>
2023-12-28 17:16:21 +0530

    display available people
    next steps:
    
    Add the time and the unavailable people

--- content_script.js
+++ content_script.js
@@ -1,13 +1,64 @@
-chrome.runtime.onMessage.addListener(showAvailability)
+chrome.runtime.onMessage.addListener(showAvailabilityManager)
 
-function showAvailability(request, sender, sendResponse)
+function showAvailabilityManager(request, sender, sendResponse)
 {
   if (request.message === 'view_people')
   {
+    showAvailability()
     sendResponse({reply : 'done'})
   }
   else
   {
     sendResponse({reply : 'UNEXPECTED MESSAGE!!!'})
+  }
+}
+
+/**
+ * Function that displays who is available at the current slot time the cursor is over
+ * Displayed as an alert message
+ */
+function showAvailability()
+{
+  let displayStr = ''
+
+  displayStr += 'AVAILBLE:\n' + getAvailablePeople() + '\n'
+
+  alert(displayStr)
+}
+
+/**
+ * 
+ * @returns The list of available people as a comma separated list
+ */
+function getAvailablePeople()
+{
+  let divOfAvailable = document.querySelector('#Available')
+  if (divOfAvailable === null)
+  {
+    return ''
+  }
+  else
+  {
+    let availableHTML = divOfAvailable.innerHTML
+    let availableNamesArray = availableHTML.split('<br>')
+
+    let availableNamesStr = availableNamesArray.reduce
+    (
+      function(acc, currName)
+      {
+        if (currName !== '')
+        {
+          if (acc === '')
+          {return currName}
+          else
+          {return acc + ', ' + currName}
+        }
+        else
+        {return acc}
+      }
+      , ''
+    )
+
+    return availableNamesStr
   }
 }
